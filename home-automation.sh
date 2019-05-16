9215fba0b8252671b7b87a992d7511af1731c3fb
Deimi <deimi@deimi.at>
2019-05-16 07:45:50 +0200

    Rename getter and setter for node-red flows Clean up of variable expansion style according to Google guidelines

--- home-automation.sh
+++ home-automation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PACKAGE_VERSION="$(<VERSION)"
-LOG_DEBUG_ACTIVE=$2
+LOG_DEBUG_ACTIVE=${2}
 
 DOCKER_BASE="./docker"
 DOCKER_FILE_NODERED="${DOCKER_BASE}/node-red/dockerfile"
@@ -15,11 +15,11 @@ function add_auto_completion() {
 #/usr/bin/env bash
 _home-automation_completions() 
 {
-  COMPREPLY=($(compgen -W "scratch getNodeRedFlows updateNodeRedFlows install start stop update uninstall version help" "${COMP_WORDS[1]}"))
+  COMPREPLY=($(compgen -W "scratch getNodeRedFiles updateNodeRedFiles install start stop update uninstall version help" "${COMP_WORDS[1]}"))
 }
 complete -F _home-automation_completions home-automation.sh'
 
-    echo "$completion_script" >/etc/bash_completion.d/home-automation
+    echo "${completion_script}" >/etc/bash_completion.d/home-automation
 }
 
 function show_help() {
@@ -27,13 +27,13 @@ function show_help() {
     echo "Copyright (c) 2019 Matthias Deimbacher under MIT license"
     echo
     echo "Usage:"
-    echo "$0 {scratch|getNodeRedFlows|updateNodeRedFlows|install|start|stop|update|uninstall|version|help|--addAutoCompletion} [-d]"
+    echo "${0} {scratch|getNodeRedFlows|updateNodeRedFlows|install|start|stop|update|uninstall|version|help|--addAutoCompletion}"
     echo
     echo "scratch ... Set up a complete home-automation system from scratch to running."
     echo "            Useful for a \"virgin\" host where the home-automation system was not installed yet"
-    echo "getNodeRedFlows ... Copies NodeRed flows from the container to the current path"
+    echo "getNodeRedFiles ... Copies NodeRed flows from the container to the current path"
     echo "                    Useful during developing the flows itself"
-    echo "updateNodeRedFlows ... Copies NodeRed flow from current repo dir to the container volume"
+    echo "updateNodeRedFiles ... Copies NodeRed flow from current repo dir to the container volume"
     echo "                       Useful for updating the NodeRed flows without rebuilding the container image"
     echo
     echo "install ... Build all necessary docker images and volumes"
@@ -48,12 +48,12 @@ function show_help() {
 }
 
 function show_version() {
-    echo $PACKAGE_VERSION
+    echo ${PACKAGE_VERSION}
 }
 
 function log_debug() {
-    if [[ "$LOG_DEBUG_ACTIVE" == "-d" ]]; then
-        echo "home-automation script: $1"
+    if [[ "${LOG_DEBUG_ACTIVE}" == "-d" ]]; then
+        echo "home-automation script: ${1}"
     fi
 }
 
@@ -78,7 +78,7 @@ function check_dependencies() {
 function detect_os() {
     local uname_os=$(uname -o)
 
-    case $uname_os in
+    case ${uname_os} in
         "Msys")
             echo "win"
             ;;
@@ -88,7 +88,7 @@ function detect_os() {
             ;;
 
         *)
-            echo $uname_os
+            echo ${uname_os}
             ;;
     esac
 }
@@ -96,7 +96,7 @@ function detect_os() {
 function detect_arch() {
     local uname_arch=$(uname -m)
 
-    case $uname_arch in
+    case ${uname_arch} in
         "x86_64")
             echo "amd64"
             ;;
@@ -106,7 +106,7 @@ function detect_arch() {
             ;;
 
         *)
-            echo $uname_arch
+            echo ${uname_arch}
             ;;
     esac
 }
@@ -117,10 +117,10 @@ function check_host_supported() {
     local detected_os=$(detect_os)
     local detected_arch=$(detect_arch)
 
-    log_debug "Detected OS: $detected_os"
-    log_debug "Detected architecture: $detected_arch"
+    log_debug "Detected OS: ${detected_os}"
+    log_debug "Detected architecture: ${detected_arch}"
 
-    case $detected_os in
+    case ${detected_os} in
         "win")
             echo "Error! Windows OS not supported"
             exit 1
@@ -131,28 +131,28 @@ function check_host_supported() {
             ;;
 
         *)
-            echo "Error! Unsupported host OS: $detected_os"
+            echo "Error! Unsupported host OS: ${detected_os}"
             exit 1
             ;;
     esac
 
-    case $detected_arch in
+    case ${detected_arch} in
         "arm" | "amd64")
             # all fine
             ;;
 
         *)
-            echo "Error! Unsupported host architecture: $detected_arch"
+            echo "Error! Unsupported host architecture: ${detected_arch}"
             exit 1
             ;;
     esac
 }
 
 function confirm_command() {
-    echo -n "Are you sure that you want to $1 (Non persistent data will be lost!!!!)? [yes|no] "
+    echo -n "Are you sure that you want to ${1} (Non persistent data will be lost!!!!)? [yes|no] "
     read confirm_choice
 
-    case $confirm_choice in
+    case ${confirm_choice} in
         "yes" | "Yes" | "y" | "Y")
             # continue
             return
@@ -171,10 +171,10 @@ function confirm_command() {
 }
 
 function get_container_status() {
-    local raw_container_status=$(docker container inspect $DOCKER_CONTAINER_NODERED | grep Status)
+    local raw_container_status=$(docker container inspect ${DOCKER_CONTAINER_NODERED} | grep Status)
     # FIXME find better solution where we won't get an error in case of a non existing container
 
-    case $raw_container_status in
+    case ${raw_container_status} in
         "            \"Status\": \"running\",")
             echo "running"
             ;;
@@ -213,7 +213,7 @@ function install_system() {
     # TODO create volume
 
     log_debug "Create container"
-    if ! docker create -p $NODE_RED_PORT:1880 --restart unless-stopped --name $DOCKER_CONTAINER_NODERED $DOCKER_IMAGE_NODERED; then
+    if ! docker create -p ${NODE_RED_PORT}:1880 --restart unless-stopped --name ${DOCKER_CONTAINER_NODERED} ${DOCKER_IMAGE_NODERED}; then
         echo "Error! Creating node-red docker container failed"
         exit 1
     fi
@@ -221,13 +221,13 @@ function install_system() {
 
 function uninstall_system() {
     # Remove container
-    if ! docker container rm $DOCKER_CONTAINER_NODERED; then
+    if ! docker container rm ${DOCKER_CONTAINER_NODERED}; then
         echo "Error! Removing node-red docker container failed"
         exit 1
     fi
 
     # Delete image and config files
-    if ! docker image rm $DOCKER_IMAGE_NODERED; then
+    if ! docker image rm ${DOCKER_IMAGE_NODERED}; then
         echo "Error! Removing node-red docker image failed"
         exit 1
     fi
@@ -237,7 +237,7 @@ function start_system() {
     log_debug "start_system"
 
     log_debug "Starting docker container"
-    if ! docker start $DOCKER_CONTAINER_NODERED; then
+    if ! docker start ${DOCKER_CONTAINER_NODERED}; then
         echo "Error! Starting node-red docker container failed"
         exit 1
     fi
@@ -247,7 +247,7 @@ function stop_system() {
     log_debug "stop_system"
 
     log_debug "Stopping docker container"
-    if ! docker stop $DOCKER_CONTAINER_NODERED; then
+    if ! docker stop ${DOCKER_CONTAINER_NODERED}; then
         echo "Error! Stopping node-ned docker container failed"
         exit 1
     fi
@@ -262,7 +262,7 @@ function update_system() {
     update_repo
     install_system
 
-    if [[ $container_status == "running" ]]; then
+    if [[ ${container_status} == "running" ]]; then
         start_system
     fi
 }
@@ -273,7 +273,7 @@ function system_from_scratch() {
     local container_status=$(get_container_status)
 
     # if container already exists then we cannot build from scratch
-    if [[ $container_status == "running" ]] || [[ $container_status == "created" ]] || [[ $container_status == "exited" ]]; then
+    if [[ ${container_status} == "running" ]] || [[ ${container_status} == "created" ]] || [[ ${container_status} == "exited" ]]; then
         echo "Error! Set up from scratch not possible because system is already installed and/or running"
         exit 1
     fi
@@ -284,13 +284,13 @@ function system_from_scratch() {
     start_system
 }
 
-function get_node_red_flows() {
+function get_node_red_files() {
     log_debug "get_node_red_flows"
 
     # TODO Copy node-red flow files from volume to current dir
 }
 
-function update_node_red_flows() {
+function update_node_red_files() {
     log_debug "update_node_red_flows"
 
     # TODO Copy node-red flow files from dir into volume
@@ -301,7 +301,7 @@ function main() {
     check_host_supported
     check_dependencies
 
-    case $1 in
+    case ${1} in
         "install")
             echo "Installing home automation system"
             install_system
@@ -334,15 +334,15 @@ function main() {
             system_from_scratch
             ;;
 
-        "getNodeRedFlows" | "getnoderedflows")
+        "getNodeRedFiles" | "getnoderedfiles")
             echo "Getting NodeRed flows from home automation system"
-            get_node_red_flows # TODO test
+            get_node_red_files # TODO test
             ;;
 
-        "updateNodeRedFlows" | "updatenoderedflows")
+        "updateNodeRedFiles" | "updatenoderedfiles")
             echo "Copy NodeRed flows to home automation system"
             confirm_command "update the nodered flows"
-            update_node_red_flows # TODO test
+            update_node_red_files # TODO test
             ;;
 
         "version" | "-v" | "--version")
@@ -368,7 +368,7 @@ function main() {
             ;;
 
         *)
-            echo "ERROR! Unknown command: $1"
+            echo "ERROR! Unknown command: ${1}"
             echo
             show_help
             exit 1
